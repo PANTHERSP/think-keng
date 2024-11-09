3d8f2a6607e1c02f973866bd1fb2d44595156749
PANTHERSP <suphakrit.panther@gmail.com>
2024-11-09 14:09:28 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // ข้าม ESLint warning/error ใน build production
+    },
+  };
+  
+  export default nextConfig;
+  
